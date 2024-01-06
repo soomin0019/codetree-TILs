e261c0df4f39805e84d6ec71db8c6a344f600ff4
LEE SOO-MIN <113010528+soomin0019@users.noreply.github.com>
2024-01-06 23:03:24 +0900

    Title: 999 또는 -999, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240106/999 또는 -999/999-or-999.java
+++ 240106/999 또는 -999/999-or-999.java
@@ -5,18 +5,19 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int[] N = new int[100];
-        int max = sc.nextInt();
-        int min = sc.nextInt();
+        int max = -998;
+        int min = 998;
+
 
         for(int i=0; i<100; i++) {
             N[i] = sc.nextInt();
             if(N[i] == 999 || N[i] == -999) {
                 break;
             }
-            if(max < N[i]) {
+            if(max <= N[i]) {
                 max = N[i];
             }
-            else if(min > N[i]) {
+            else if(min >= N[i]) {
                 min = N[i];
             }
         }
