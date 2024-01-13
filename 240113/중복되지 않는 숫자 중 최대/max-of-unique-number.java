655a1a695e202e6ce1d89027cc6242578e8d19e1
LEE SOO-MIN <113010528+soomin0019@users.noreply.github.com>
2024-01-13 22:37:44 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240113/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240113/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -14,7 +14,7 @@ public class Main {
         }
 
         for(int i=0; i<n; i++) {
-            for(int j=0; j<i; j++) {
+            for(int j=1; j<=i; j++) {
                 if((N[i] != N[j]) && (max < N[i])) {
                     max = N[i];
                 }
