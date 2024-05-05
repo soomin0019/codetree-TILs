422e25addcaeb72171042101733ab8ffeda01483
LEE SOO-MIN <113010528+soomin0019@users.noreply.github.com>
2024-05-05 17:18:13 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 245ms, Memory: 13MB, Status: Passed - CodeTree

--- 240505/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 240505/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -1,30 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int n = sc.nextInt();
         int[] a = new int[1000];
 
-        for(int i=0; i<N; i++) {
+        for(int i=0; i< n; i++) {
             a[i] = sc.nextInt();
         }
 
-        int idx = N;
+        int idx = n;
         while(true) {
-            int midx = 0;
-
+            int last = 0;
             for(int i=0; i<idx; i++) {
-                if(a[i]> a[midx])
-                    midx = i;
+                if(a[i] > a[last])
+                    last = i;
             }
-            System.out.printf((midx+1) + " ");
+            System.out.printf( (last + 1) + " ");
 
-            if(midx == 0) {
+            if(last == 0)
                 break;
-            }
-            idx = midx;
+
+            idx = last;
         }
 
     }
